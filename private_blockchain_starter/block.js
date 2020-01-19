9f7d0265209ac559de97b6386e1a859b4243fd52
Diprotiv Sarkar <diprotiv.ds@gmail.com>
2020-01-19 23:50:58 +0530

    Modified the Block exercise

--- private_blockchain_starter/block.js
+++ private_blockchain_starter/block.js
@@ -3,6 +3,16 @@
 * that were already discussed in the class.
 */
 class Block {
+    constructor(data) {
+        /**
+         * TODO: Add the headers.
+         */
+    }
+    generateHash() {
+        /**
+         * TODO: Add the hashing implementation and return a new Promise.
+         */
+    }
 }
 
 module.exports = Block
